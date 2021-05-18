58ba1fed4130e63b929fd823a24cd6ac08475976
aalassi <aalassi@yahoo.com>
2021-05-18 18:39:11 +0200

    first commit

---
+++ js/hangman.js
@@ -0,0 +1,89 @@
+var programming_languages = [
+	"pussy",
+	"kleine pussy",
+	"meine kleine pussy"
+]
+
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
+
+function randomWord() {
+  answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
+}
+
+function generateButtons() {
+  let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz '.split('').map(letter =>
+    `
+      <button
+        class="btn btn-lg btn-primary m-2"
+        id='` + letter + `'
+        onClick="handleGuess('` + letter + `')"
+      >
+        ` + letter + `
+      </button>
+    `).join('');
+
+  document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
+
+function handleGuess(chosenLetter) {
+  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+  document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+  if (answer.indexOf(chosenLetter) >= 0) {
+    guessedWord();
+    checkIfGameWon();
+  } else if (answer.indexOf(chosenLetter) === -1) {
+    mistakes++;
+    updateMistakes();
+    checkIfGameLost();
+    updateHangmanPicture();
+  }
+}
+
+function updateHangmanPicture() {
+  document.getElementById('hangmanPic').src = './images/' + mistakes + '.jpg';
+}
+
+function checkIfGameWon() {
+  if (wordStatus === answer) {
+    document.getElementById('keyboard').innerHTML = 'mein süßer Boss :-) du hast gewonnen!!!';
+  }
+}
+
+function checkIfGameLost() {
+  if (mistakes === maxWrong) {
+    document.getElementById('wordSpotlight').innerHTML = 'die richtige Antwort war: ' + answer;
+    document.getElementById('keyboard').innerHTML = 'versuch es nochmal Böser Boss xD !!!';
+  }
+}
+
+function guessedWord() {
+  wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+  document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
+function updateMistakes() {
+  document.getElementById('mistakes').innerHTML = mistakes;
+}
+
+function reset() {
+  mistakes = 0;
+  guessed = [];
+  document.getElementById('hangmanPic').src = './images/0.jpg';
+
+  randomWord();
+  guessedWord();
+  updateMistakes();
+  generateButtons();
+}
+
+document.getElementById('maxWrong').innerHTML = maxWrong;
+
+randomWord();
+generateButtons();
+guessedWord();
